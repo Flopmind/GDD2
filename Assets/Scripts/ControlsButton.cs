8343663f6f6be0dacab144d2efe78fac1ee3c0de
Ryan Volz (RIT Student) <rjv8806@ad.rit.edu>
2018-10-29 13:33:05 -0400

    Add main menu scene (still need Controls and Credits scenes

---
+++ Assets/Scripts/ControlsButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlsButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("ControlsScene");
+    }
+}
